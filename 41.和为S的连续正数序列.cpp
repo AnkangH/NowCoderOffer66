77dfb9090e9e28bb095b7a7c31fc65cc72478846
侯安康 <1611506326@qq.com>
2019-06-23 16:20:45 +0800

    Create 41.和为S的连续正数序列.cpp

---
+++ 41.和为S的连续正数序列.cpp
@@ -0,0 +1,28 @@
+/*
+输出所有和为S的连续正数序列。序列内按照从小至大的顺序，序列间按照开始数字从小到大的顺序。序列至少包含两个数字。
+*/
+
+class Solution {
+public:
+    vector<vector<int> > FindContinuousSequence(int sum) {
+        vector<vector<int>> res;
+        for(int i=1;i<=sum;i++)
+            dfs(res,{},sum,i);//从i开始dfs连续数字
+        return res;
+    }
+    void dfs(vector<vector<int>>& res,vector<int> temp,int sum,int index)
+    {
+        if(index>sum)
+            return;//全为正数 当前和若大于sum 则其后必不满足
+        temp.push_back(index);//index<=sum 当前数字放入组合
+        if(index==sum)//满足连续和
+        {
+            int size=temp.size();
+            if(size<=1)//题目要求至少两个数
+                return;//排除掉一个数等于sum的情况
+            res.push_back(temp);
+            return;
+        }
+        dfs(res,temp,sum-index,index+1);//不满足连续和 继续找相邻数字
+    }
+};
